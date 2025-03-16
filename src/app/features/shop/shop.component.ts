2d8d7bd51b77d96bac77753199573114dbdc55e4
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-16 21:08:26 +0000

    add navbar component

--- src/app/features/shop/shop.component.ts
+++ src/app/features/shop/shop.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-shop',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
