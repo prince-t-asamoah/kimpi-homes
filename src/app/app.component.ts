740e1ec8918d6e693008198dbc218788205a1be7
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-16 19:25:04 +0000

    install and setup font awesome icon library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor(private readonly meta: Meta) {
+  constructor(
+    private readonly meta: Meta,
+    private readonly faIconLibrary: FaIconLibrary
+  ) {
     this.meta.addTag({
       name: 'description',
       content: 'Quality Furniture for Every Home & Office',
     });
+
+    this.faIconLibrary.addIconPacks(fas);
   }
 }
