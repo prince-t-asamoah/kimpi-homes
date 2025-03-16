f1ce8a2b6c2e9e3080614f64b8a8c369b7d7dfdc
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-16 20:54:11 +0000

    create shop page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { ShopComponent } from './features/shop/shop.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full', title: 'Kimpi Homes' },
   { path: 'home', component: HomeComponent, title: 'Kimpi Homes | Home' },
+  { path: 'shop', component: ShopComponent, title: 'Kimpi Homes | Shop' },
 ];
---
+++ src/app/features/shop/shop.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  imports: [],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss',
+})
+export class ShopComponent {}
