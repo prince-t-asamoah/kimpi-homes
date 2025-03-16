24a8b02e4d3f5c4272705a6f97f4da75eb2c9d09
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-16 16:35:50 +0000

    create homepage component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full', title: 'Kimpi Homes' },
+  { path: 'home', component: HomeComponent, title: 'Kimpi Homes | Home' },
+];
