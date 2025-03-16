98044cb59b291060989690d1416ed623af6b5d81
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-16 12:23:06 +0000

    update component to use app routes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'kimpi-homes' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('kimpi-homes');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, Kimpi Homes'
-    );
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'kimpi-homes';
+  constructor(private readonly meta: Meta) {
+    this.meta.addTag({
+      name: 'description',
+      content: 'Quality Furniture for Every Home & Office',
+    });
+  }
 }
