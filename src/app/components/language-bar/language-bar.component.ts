0d35c1390b1e02162616863ef194746f4a1e579d
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-18 21:15:08 +0000

    import font awesome icon module

--- src/app/components/language-bar/language-bar.component.ts
+++ src/app/components/language-bar/language-bar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-language-bar',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './language-bar.component.html',
   styleUrl: './language-bar.component.scss',
 })
