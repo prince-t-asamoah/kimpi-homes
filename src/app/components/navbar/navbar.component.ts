c67a86b62d3dbca6442455e33775eebca9696ef8
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-18 21:17:39 +0000

    use language bar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,10 +2,11 @@ import { NgClass } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LanguageBarComponent } from '../language-bar/language-bar.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgClass, FontAwesomeModule, RouterModule],
+  imports: [NgClass, FontAwesomeModule, RouterModule, LanguageBarComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   encapsulation: ViewEncapsulation.None,
