f9f0f924e277a1b4e84ffe78fdc93bf9af6ce18c
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2025-03-18 21:02:41 +0000

    create component for language translation

---
+++ src/app/components/language-bar/language-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-bar',
+  imports: [],
+  templateUrl: './language-bar.component.html',
+  styleUrl: './language-bar.component.scss',
+})
+export class LanguageBarComponent {}
