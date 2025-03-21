4bab2e66eb4e58c6b8ffd3598f4c0cdab4528165
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-03-21 23:49:34 +0000

    use internationlization data

--- src/app/components/language-bar/language-bar.component.ts
+++ src/app/components/language-bar/language-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { countries, languages } from '../../data/internalization';
 
 @Component({
   selector: 'app-language-bar',
@@ -7,4 +8,20 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   templateUrl: './language-bar.component.html',
   styleUrl: './language-bar.component.scss',
 })
-export class LanguageBarComponent {}
+export class LanguageBarComponent {
+  languageBarDialog =
+    viewChild<ElementRef<HTMLDialogElement>>('languageBarDialog');
+  countries = countries;
+  languages = languages;
+
+  openDialog(): void {
+    if (this.languageBarDialog()?.nativeElement.open) {
+      this.languageBarDialog()?.nativeElement.close();
+    }
+    this.languageBarDialog()?.nativeElement.showModal();
+  }
+
+  closeDialog(): void {
+    this.languageBarDialog()?.nativeElement.close();
+  }
+}
