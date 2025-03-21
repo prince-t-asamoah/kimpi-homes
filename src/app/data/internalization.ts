ceb111f5f09ac84b6d0410ed73c2054efb8201e5
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-03-21 22:54:57 +0000

    add internalization data for language bar form

---
+++ src/app/data/internalization.ts
@@ -0,0 +1,21 @@
+export const countries = [
+  { name: 'Austria', defaultLang: 'de' },
+  { name: 'Belgium', defaultLang: 'fr' },
+  { name: 'Germany', defaultLang: 'de' },
+  { name: 'Switzerland', defaultLang: 'de' },
+  { name: 'France', defaultLang: 'fr' },
+  { name: 'Italy', defaultLang: 'en' },
+  { name: 'Spain', defaultLang: 'en' },
+  { name: 'Nigeria', defaultLang: 'en' },
+  { name: 'South Africa', defaultLang: 'en' },
+  { name: 'Ghana', defaultLang: 'en' },
+  { name: 'United States', defaultLang: 'en' },
+  { name: 'Bolivia', defaultLang: 'en' },
+  { name: 'Brazil', defaultLang: 'en' },
+  { name: 'Canada', defaultLang: 'fr' },
+];
+
+export const languages = [
+  { id: 'en', name: 'English' },
+  { id: 'de', name: 'Deutch' },
+];
