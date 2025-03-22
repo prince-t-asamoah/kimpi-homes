e9e9703c16cfccd35761daca0dc5fe5176aa0063
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-03-22 00:57:22 +0000

    add test for opening and closing internationalization dialog form

--- src/app/components/language-bar/language-bar.component.spec.ts
+++ src/app/components/language-bar/language-bar.component.spec.ts
@@ -2,11 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LanguageBarComponent } from './language-bar.component';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { DebugElement, ElementRef, Signal } from '@angular/core';
 
 describe('LanguageBarComponent', () => {
   let component: LanguageBarComponent;
   let fixture: ComponentFixture<LanguageBarComponent>;
   let faIconLibrary: FaIconLibrary;
+  let internationalizationForm: Signal<
+    ElementRef<HTMLDialogElement> | undefined
+  >;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,6 +21,8 @@ describe('LanguageBarComponent', () => {
     fixture = TestBed.createComponent(LanguageBarComponent);
     faIconLibrary = TestBed.inject(FaIconLibrary);
     component = fixture.componentInstance;
+    internationalizationForm = fixture.componentInstance.languageBarDialog;
+    debugElement = fixture.debugElement;
 
     faIconLibrary.addIconPacks(fas);
     fixture.detectChanges();
@@ -26,6 +33,25 @@ describe('LanguageBarComponent', () => {
   });
 
   it('should have internationlization form dialog', () => {
-    expect(component.languageBarDialog).toBeTruthy();
+    expect(internationalizationForm()).toBeTruthy();
+  });
+
+  it('should verify open internationlization form button exist', () => {
+    const openButtonEl = debugElement.nativeElement.querySelector(
+      '#openLanguageFormButton'
+    );
+    expect(openButtonEl).toBeTruthy();
+  });
+
+  it('should show internationlization form when open internationalization button is clicked', () => {
+    component.openLanguageForm();
+    fixture.detectChanges();
+    expect(internationalizationForm()?.nativeElement.open).toEqual(true);
+  });
+
+  it('should close internationlization form when close internationalization button is clicked', () => {
+    component.closeLanguageForm();
+    fixture.detectChanges();
+    expect(internationalizationForm()?.nativeElement.open).toEqual(false);
   });
 });
