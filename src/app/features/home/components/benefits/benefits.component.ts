229e42fef45dc6c2037c6f4b115ae0d46e4dab7a
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-03-24 13:25:14 +0000

    create company benefits component

---
+++ src/app/features/home/components/benefits/benefits.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-benefits',
+  imports: [],
+  templateUrl: './benefits.component.html',
+  styleUrl: './benefits.component.scss',
+})
+export class BenefitsComponent {}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { BenefitsComponent } from './components/benefits/benefits.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, BenefitsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
