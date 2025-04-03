64ecc2bbbc0b9cdc2392ad02af1e876016fb87be
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-04-03 12:32:30 +0000

    create and export icon library data

---
+++ src/app/data/icon-library.ts
@@ -0,0 +1,29 @@
+import {
+  Trophy,
+  Armchair,
+  Truck,
+  Banknote,
+  ChevronRight,
+  ChevronLeft,
+  Search,
+  User,
+  ShoppingCart,
+  X,
+  Globe,
+} from 'lucide-angular';
+
+const iconLibrary = {
+  Trophy,
+  Armchair,
+  Truck,
+  Banknote,
+  ChevronRight,
+  ChevronLeft,
+  Search,
+  User,
+  ShoppingCart,
+  X,
+  Globe,
+};
+
+export default iconLibrary;
