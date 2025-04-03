2429f19aa800dea056ee6663edef3614b6f9d1c5
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-04-03 09:05:40 +0000

    remove font awesome icon pack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +9,10 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor(
-    private readonly meta: Meta,
-    private readonly faIconLibrary: FaIconLibrary
-  ) {
+  constructor(private readonly meta: Meta) {
     this.meta.addTag({
       name: 'description',
       content: 'Quality Furniture for Every Home & Office',
     });
-
-    this.faIconLibrary.addIconPacks(fas);
   }
 }
