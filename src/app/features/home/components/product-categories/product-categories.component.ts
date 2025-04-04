4d74f94efd24b2662402e056d957cc984825235a
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-04-04 13:06:48 +0000

    create product categories component

---
+++ src/app/features/home/components/product-categories/product-categories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-product-categories',
+  imports: [LucideAngularModule],
+  templateUrl: './product-categories.component.html',
+  styleUrl: './product-categories.component.scss',
+})
+export class ProductCategoriesComponent {}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { BenefitsComponent } from './components/benefits/benefits.component';
+import { ProductCategoriesComponent } from './components/product-categories/product-categories.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent, BenefitsComponent],
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    BenefitsComponent,
+    ProductCategoriesComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
