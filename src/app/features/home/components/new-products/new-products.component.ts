86f96dbfeb524248d1f4279d1e03c4d60d4b0964
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-04-08 13:47:09 +0000

    create new product arrivals section component

---
+++ src/app/features/home/components/new-products/new-products.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-products',
+  imports: [],
+  templateUrl: './new-products.component.html',
+  styleUrl: './new-products.component.scss',
+})
+export class NewProductsComponent {}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,6 +3,7 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { BenefitsComponent } from './components/benefits/benefits.component';
 import { ProductCategoriesComponent } from './components/product-categories/product-categories.component';
+import { NewProductsComponent } from './components/new-products/new-products.component';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { ProductCategoriesComponent } from './components/product-categories/prod
     HeroComponent,
     BenefitsComponent,
     ProductCategoriesComponent,
+    NewProductsComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
