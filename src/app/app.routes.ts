2820a0fbb0e280733d6b0884efe46575ed094ebf
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-04-16 16:27:41 +0000

    create about us page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
+import { AboutUsComponent } from './features/about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full', title: 'Kimpi Homes' },
   { path: 'home', component: HomeComponent, title: 'Kimpi Homes | Home' },
   { path: 'shop', component: ShopComponent, title: 'Kimpi Homes | Shop' },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+    title: 'Kimpi Homes | About Us',
+  },
 ];
