e383b9247575826c58eea7f3dbbde82cc6918260
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-01 20:49:46 +0000

    update imports and styleurl

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['../auth.scss'],
   encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent {}
