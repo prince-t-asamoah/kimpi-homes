19062510041c133590dce15de6e3fe36a15edac6
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-04 10:25:31 +0000

    create forget password page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ShopComponent } from './features/shop/shop.component';
 import { AboutUsComponent } from './features/about-us/about-us.component';
 import { LoginComponent } from './features/auth/login/login.component';
 import { CreateAccountComponent } from './features/auth/create-account/create-account.component';
+import { ForgetPasswordComponent } from './features/auth/forget-password/forget-password.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full', title: 'Kimpi Homes' },
@@ -23,5 +24,10 @@ export const routes: Routes = [
     path: 'create-account',
     component: CreateAccountComponent,
     title: 'Kimpi Homes | Create Account',
+  },
+  {
+    path: 'forget-password',
+    component: ForgetPasswordComponent,
+    title: 'Kimpi Homes | Forget Password',
   },
 ];
