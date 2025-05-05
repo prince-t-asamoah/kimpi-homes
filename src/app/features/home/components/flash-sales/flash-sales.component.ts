d1fe93fed55536306470d589d7d3d05f452a3cf2
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-05 20:41:15 +0000

    create a product flash sales component

---
+++ src/app/features/home/components/flash-sales/flash-sales.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flash-sales',
+  imports: [],
+  templateUrl: './flash-sales.component.html',
+  styleUrl: './flash-sales.component.scss',
+})
+export class FlashSalesComponent {}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { BenefitsComponent } from './components/benefits/benefits.component';
-import { ProductCategoriesComponent } from './components/product-categories/product-categories.component';
+import { ProductCategoriesComponent } from '../../components/product-categories/product-categories.component';
 import { NewProductsComponent } from './components/new-products/new-products.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { LanguageBarComponent } from '../../components/language-bar/language-bar.component';
+import { FlashSalesComponent } from './components/flash-sales/flash-sales.component';
 
 @Component({
   selector: 'app-home',
@@ -17,6 +18,7 @@ import { LanguageBarComponent } from '../../components/language-bar/language-bar
     NewProductsComponent,
     FooterComponent,
     LanguageBarComponent,
+    FlashSalesComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
