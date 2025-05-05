27dfe97b80616329120d89b359844b25ac6efcea
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-05 18:38:24 +0000

    add language bar and footer component

--- src/app/features/shop/shop.component.ts
+++ src/app/features/shop/shop.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { LanguageBarComponent } from '../../components/language-bar/language-bar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-shop',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, LanguageBarComponent, FooterComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
