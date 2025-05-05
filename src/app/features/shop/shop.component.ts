f1147e3f8b70314b0a1b6ef075cd23afe06a22b4
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-05 21:13:05 +0000

    use product categories component

--- src/app/features/shop/shop.component.ts
+++ src/app/features/shop/shop.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { LanguageBarComponent } from '../../components/language-bar/language-bar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ProductCategoriesComponent } from '../../components/product-categories/product-categories.component';
 
 @Component({
   selector: 'app-shop',
-  imports: [NavbarComponent, LanguageBarComponent, FooterComponent],
+  imports: [
+    NavbarComponent,
+    LanguageBarComponent,
+    FooterComponent,
+    ProductCategoriesComponent,
+  ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
