e0c6834f14eb65d125be45aee9d54589a94db2bd
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-06 07:51:21 +0000

    add product filter button icon

--- src/app/features/shop/components/product-listings/product-listings.component.ts
+++ src/app/features/shop/components/product-listings/product-listings.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-product-listings',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './product-listings.component.html',
   styleUrl: './product-listings.component.scss',
 })
