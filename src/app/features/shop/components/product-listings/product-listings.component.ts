7bd4a4db87bf7501df11009ac27c46772f454b61
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-06 07:17:06 +0000

    create product listings component

---
+++ src/app/features/shop/components/product-listings/product-listings.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-listings',
+  imports: [],
+  templateUrl: './product-listings.component.html',
+  styleUrl: './product-listings.component.scss',
+})
+export class ProductListingsComponent {}
