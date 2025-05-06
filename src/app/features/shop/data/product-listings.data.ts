10f368954cd123c4b239dbb765cbb775e22c0c34
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-06 12:28:50 +0000

    add sample product image

--- src/app/features/shop/data/product-listings.data.ts
+++ src/app/features/shop/data/product-listings.data.ts
@@ -4,6 +4,7 @@ const productListingsData: Product[] = [
   {
     id: '1',
     name: 'Mid-Century Modern Sofa',
+    imageUrl: './assets/images/shop/product-image.png',
     price: 899,
     ratings: 4.7,
     reviews: [],
@@ -11,6 +12,7 @@ const productListingsData: Product[] = [
   {
     id: '2',
     name: 'Solid Wood Dining Table',
+    imageUrl: './assets/images/shop/product-image.png',
     price: 1199,
     ratings: 4.9,
     reviews: [],
@@ -18,6 +20,7 @@ const productListingsData: Product[] = [
   {
     id: '3',
     name: 'Ergonomic Office Chair',
+    imageUrl: './assets/images/shop/product-image.png',
     price: 299,
     ratings: 4.5,
     reviews: [],
@@ -25,6 +28,7 @@ const productListingsData: Product[] = [
   {
     id: '4',
     name: 'Rustic Queen Bed Frame',
+    imageUrl: './assets/images/shop/product-image.png',
     price: 749,
     ratings: 4.6,
     reviews: [],
@@ -32,6 +36,7 @@ const productListingsData: Product[] = [
   {
     id: '5',
     name: 'Minimalist TV Stand',
+    imageUrl: './assets/images/shop/product-image.png',
     price: 499,
     ratings: 4.4,
     reviews: [],
