439fa356fac886f360275b5a0d310afc06bfc691
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-06 05:39:31 +0000

    import lucide icon and router module

--- src/app/features/shop/shop.component.ts
+++ src/app/features/shop/shop.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { LanguageBarComponent } from '../../components/language-bar/language-bar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
@@ -7,6 +9,8 @@ import { ProductCategoriesComponent } from '../../components/product-categories/
 @Component({
   selector: 'app-shop',
   imports: [
+    RouterModule,
+    LucideAngularModule,
     NavbarComponent,
     LanguageBarComponent,
     FooterComponent,
