3fd19106e7c247cbffeb2bcff955dfbd72f635a9
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-06 06:46:38 +0000

    set component encapsulation to none

--- src/app/components/product-categories/product-categories.component.ts
+++ src/app/components/product-categories/product-categories.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
@@ -6,5 +6,6 @@ import { LucideAngularModule } from 'lucide-angular';
   imports: [LucideAngularModule],
   templateUrl: './product-categories.component.html',
   styleUrl: './product-categories.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProductCategoriesComponent {}
