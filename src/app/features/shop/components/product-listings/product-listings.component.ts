364e81ca532f751a93f712426acd71e418fec0df
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-06 11:45:13 +0000

    create sample data for product listings

--- src/app/features/shop/components/product-listings/product-listings.component.ts
+++ src/app/features/shop/components/product-listings/product-listings.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
+import { ProductListingsCardComponent } from '../product-listings-card/product-listings-card.component';
+import productListingsData from '../../data/product-listings.data';
 
 @Component({
   selector: 'app-product-listings',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, ProductListingsCardComponent],
   templateUrl: './product-listings.component.html',
   styleUrl: './product-listings.component.scss',
 })
-export class ProductListingsComponent {}
+export class ProductListingsComponent {
+  productListings = productListingsData;
+}
---
+++ src/app/features/shop/data/product-listings.data.ts
@@ -0,0 +1,41 @@
+import { Product } from '../model/product-listings.model';
+
+const productListingsData: Product[] = [
+  {
+    id: '1',
+    name: 'Mid-Century Modern Sofa',
+    price: 899,
+    ratings: 4.7,
+    reviews: [],
+  },
+  {
+    id: '2',
+    name: 'Solid Wood Dining Table',
+    price: 1199,
+    ratings: 4.9,
+    reviews: [],
+  },
+  {
+    id: '3',
+    name: 'Ergonomic Office Chair',
+    price: 299,
+    ratings: 4.5,
+    reviews: [],
+  },
+  {
+    id: '4',
+    name: 'Rustic Queen Bed Frame',
+    price: 749,
+    ratings: 4.6,
+    reviews: [],
+  },
+  {
+    id: '5',
+    name: 'Minimalist TV Stand',
+    price: 499,
+    ratings: 4.4,
+    reviews: [],
+  },
+];
+
+export default productListingsData;
