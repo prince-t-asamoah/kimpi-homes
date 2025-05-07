8700d4df383edda21ad21cc68e79261e811c9c4d
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-07 16:27:09 +0000

    create product filter component

---
+++ src/app/features/shop/components/product-filter/product-filter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-filter',
+  imports: [],
+  templateUrl: './product-filter.component.html',
+  styleUrl: './product-filter.component.scss',
+})
+export class ProductFilterComponent {}
