495e757ead1f0f922f239dab54b052ab8033a116
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-11 15:30:22 +0000

    create shop home component

---
+++ src/app/features/shop/components/shop-home/shop-home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-home',
+  imports: [],
+  templateUrl: './shop-home.component.html',
+  styleUrl: './shop-home.component.scss',
+})
+export class ShopHomeComponent {}
