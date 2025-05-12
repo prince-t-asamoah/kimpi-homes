2be8eeff52083cc96d6ecb0dc0d08cc400e9030e
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-12 20:50:17 +0000

    create filterName input prop

--- src/app/features/shop/components/product-filter/product-filter.component.ts
+++ src/app/features/shop/components/product-filter/product-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-filter',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './product-filter.component.html',
   styleUrl: './product-filter.component.scss',
 })
-export class ProductFilterComponent {}
+export class ProductFilterComponent {
+  @Input({ required: true }) filterName!: string;
+}
