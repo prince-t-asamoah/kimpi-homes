54af7b27ba6fb6bfd9716acf0bd7b7ad9ac538a6
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-12 21:39:55 +0000

    add filter sample data and filter change method

--- src/app/features/shop/components/product-filter-sidebar/product-filter-sidebar.component.ts
+++ src/app/features/shop/components/product-filter-sidebar/product-filter-sidebar.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { ProductFilterComponent } from '../product-filter/product-filter.component';
+import { ProductFilterOptionComponent } from '../product-filter-option/product-filter-option.component';
+import {
+  categoriesFilter,
+  sizeDimensionFilter,
+  availabilityFilter,
+  pricingFilter,
+  sortingFilter,
+} from '../../data/product-filter-types.data';
 
 @Component({
   selector: 'app-product-filter-sidebar',
-  imports: [],
+  imports: [ProductFilterComponent, ProductFilterOptionComponent],
   templateUrl: './product-filter-sidebar.component.html',
   styleUrl: './product-filter-sidebar.component.scss',
 })
-export class ProductFilterSidebarComponent {}
+export class ProductFilterSidebarComponent {
+  categoriesFilter = categoriesFilter;
+  sizeDimensionFilter = sizeDimensionFilter;
+  availabilityFilter = availabilityFilter;
+  priceFilter = pricingFilter;
+  sortByFilter = sortingFilter;
+
+  onFilterChange(): void {
+    // Handle filter change logic here
+  }
+}
