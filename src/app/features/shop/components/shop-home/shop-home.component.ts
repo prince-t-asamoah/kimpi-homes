0a6f72a1d4d44902c94b1fdf842d5e050076c9c6
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-12 20:32:33 +0000

    import product listings and product categories

--- src/app/features/shop/components/shop-home/shop-home.component.ts
+++ src/app/features/shop/components/shop-home/shop-home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductListingsComponent } from '../product-listings/product-listings.component';
+import { ProductCategoriesComponent } from '../../../../components/product-categories/product-categories.component';
 
 @Component({
   selector: 'app-shop-home',
-  imports: [],
+  imports: [ProductListingsComponent, ProductCategoriesComponent],
   templateUrl: './shop-home.component.html',
   styleUrl: './shop-home.component.scss',
 })
