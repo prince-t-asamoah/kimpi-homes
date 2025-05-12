a60ecea99829764f8d7922bf56a5d65d641d37bc
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-12 21:15:12 +0000

    create product filter sidebar

---
+++ src/app/features/shop/components/product-filter-sidebar/product-filter-sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-filter-sidebar',
+  imports: [],
+  templateUrl: './product-filter-sidebar.component.html',
+  styleUrl: './product-filter-sidebar.component.scss',
+})
+export class ProductFilterSidebarComponent {}
