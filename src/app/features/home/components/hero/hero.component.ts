6833353b28ec6fbd6f672dab1986496f374472d2
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-12 18:37:58 +0000

    import angular router module

--- src/app/features/home/components/hero/hero.component.ts
+++ src/app/features/home/components/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-hero',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, RouterModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
