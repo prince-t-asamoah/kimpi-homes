e5b12afc923523911f27b4cdb132ae2192445af9
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-12 21:42:02 +0000

    use product filter sidebar component

--- src/app/features/shop/components/product-listings/product-listings.component.ts
+++ src/app/features/shop/components/product-listings/product-listings.component.ts
@@ -3,24 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { LucideAngularModule } from 'lucide-angular';
 import { ProductListingsCardComponent } from '../product-listings-card/product-listings-card.component';
-import { ProductFilterComponent } from '../product-filter/product-filter.component';
 import productListingsData from '../../data/product-listings.data';
-import {
-  categoriesFilter,
-  pricingFilter,
-  availabilityFilter,
-  sizeDimensionFilter,
-  sortingFilter,
-} from '../../data/product-filter-types.data';
-import { ProductFilterOptionComponent } from '../product-filter-option/product-filter-option.component';
+
+import { ProductFilterSidebarComponent } from '../product-filter-sidebar/product-filter-sidebar.component';
 
 @Component({
   selector: 'app-product-listings',
   imports: [
     LucideAngularModule,
     ProductListingsCardComponent,
-    ProductFilterComponent,
-    ProductFilterOptionComponent,
+    ProductFilterSidebarComponent,
     CommonModule,
   ],
   templateUrl: './product-listings.component.html',
@@ -28,13 +20,4 @@ import { ProductFilterOptionComponent } from '../product-filter-option/product-f
 })
 export class ProductListingsComponent {
   productListings = productListingsData;
-  categoriesFilter = categoriesFilter;
-  sizeDimensionFilter = sizeDimensionFilter;
-  availabilityFilter = availabilityFilter;
-  priceFilter = pricingFilter;
-  sortByFilter = sortingFilter;
-
-  onFilterChange(): void {
-    // Handle filter change logic here
-  }
 }
