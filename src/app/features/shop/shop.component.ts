5718b9a48ad4e9109adfb13b37ee773f26332234
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-12 20:21:36 +0000

    import shop breadcrumb components

--- src/app/features/shop/shop.component.ts
+++ src/app/features/shop/shop.component.ts
@@ -4,8 +4,7 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { LanguageBarComponent } from '../../components/language-bar/language-bar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { ProductCategoriesComponent } from '../../components/product-categories/product-categories.component';
-import { ProductListingsComponent } from './components/product-listings/product-listings.component';
+import { ShopBreadcrumbComponent } from './components/shop-breadcrumb/shop-breadcrumb.component';
 
 @Component({
   selector: 'app-shop',
@@ -15,8 +14,7 @@ import { ProductListingsComponent } from './components/product-listings/product-
     NavbarComponent,
     LanguageBarComponent,
     FooterComponent,
-    ProductCategoriesComponent,
-    ProductListingsComponent,
+    ShopBreadcrumbComponent,
   ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
