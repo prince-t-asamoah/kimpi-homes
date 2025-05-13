344f04daa60d117ced52ab3b2dca0b51cc1f602f
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-13 11:27:10 +0000

    hide sidebar on large screen

--- src/app/features/shop/shop.component.ts
+++ src/app/features/shop/shop.component.ts
@@ -1,10 +1,16 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { LanguageBarComponent } from '../../components/language-bar/language-bar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ShopBreadcrumbComponent } from './components/shop-breadcrumb/shop-breadcrumb.component';
+import { UiService } from '../../services/ui/ui.service';
 
 @Component({
   selector: 'app-shop',
@@ -20,4 +26,19 @@ import { ShopBreadcrumbComponent } from './components/shop-breadcrumb/shop-bread
   styleUrl: './shop.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class ShopComponent {}
+export class ShopComponent {
+  private _uiService = inject(UiService);
+  isFilterSidebarOpen = this._uiService.isFilterSidebarOpen;
+  mobilePageWidth = 768; //Mobile Screen Max Width
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const windowTarget = event.target as Window;
+    if (
+      windowTarget.innerWidth > 768 &&
+      this._uiService.isFilterSidebarOpen()
+    ) {
+      this._uiService.toggleFilterSidebar();
+    }
+  }
+}
