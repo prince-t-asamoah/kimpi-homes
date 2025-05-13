a8b775315d480fcc665385c4782c217f3da9e7b7
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-13 07:58:18 +0000

    import lucide icon module

--- src/app/features/shop/components/product-filter-button/product-filter-button.component.ts
+++ src/app/features/shop/components/product-filter-button/product-filter-button.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-product-filter-button',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './product-filter-button.component.html',
   styleUrl: './product-filter-button.component.scss',
 })
