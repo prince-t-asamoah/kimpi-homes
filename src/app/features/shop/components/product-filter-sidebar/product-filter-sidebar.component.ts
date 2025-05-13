09e33a5af685a47973ffcc53bd5c86e73b7149d3
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-13 13:35:51 +0000

    implement toggle sidebar with modal on mobile screens

--- src/app/features/shop/components/product-filter-sidebar/product-filter-sidebar.component.ts
+++ src/app/features/shop/components/product-filter-sidebar/product-filter-sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { ProductFilterComponent } from '../product-filter/product-filter.component';
 import { ProductFilterOptionComponent } from '../product-filter-option/product-filter-option.component';
 import {
@@ -8,21 +9,29 @@ import {
   pricingFilter,
   sortingFilter,
 } from '../../data/product-filter-types.data';
+import { UiService } from '../../../../services/ui/ui.service';
 
 @Component({
   selector: 'app-product-filter-sidebar',
-  imports: [ProductFilterComponent, ProductFilterOptionComponent],
+  imports: [NgClass, ProductFilterComponent, ProductFilterOptionComponent],
   templateUrl: './product-filter-sidebar.component.html',
   styleUrl: './product-filter-sidebar.component.scss',
 })
 export class ProductFilterSidebarComponent {
+  uiService = inject(UiService);
   categoriesFilter = categoriesFilter;
   sizeDimensionFilter = sizeDimensionFilter;
   availabilityFilter = availabilityFilter;
   priceFilter = pricingFilter;
   sortByFilter = sortingFilter;
 
+  isSidebarOpen = this.uiService.isFilterSidebarOpen;
+
   onFilterChange(): void {
     // Handle filter change logic here
+  }
+
+  toggleSidebarOnMobile(): void {
+    this.uiService.toggleFilterSidebar();
   }
 }
