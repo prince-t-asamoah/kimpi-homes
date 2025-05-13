f76287b7293f1fbf426f73541b3d590548a52f3a
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-13 13:41:53 +0000

    implement toggle product filter sidebar

--- src/app/services/ui/ui.service.ts
+++ src/app/services/ui/ui.service.ts
@@ -1,6 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UiService {}
+export class UiService {
+  isFilterSidebarOpen = signal(false);
+
+  toggleFilterSidebar(): void {
+    this.isFilterSidebarOpen.update(prev => !prev);
+  }
+}
