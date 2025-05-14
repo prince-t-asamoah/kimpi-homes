8e8b9c65026a62c92582628e4bf8911c0585e31a
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-14 01:03:01 +0000

    implement new arrivals product listings

--- src/app/features/shop/components/shop-new-arrivals/shop-new-arrivals.component.ts
+++ src/app/features/shop/components/shop-new-arrivals/shop-new-arrivals.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { ProductFilterSidebarComponent } from '../product-filter-sidebar/product-filter-sidebar.component';
+import { ProductFilterButtonComponent } from '../product-filter-button/product-filter-button.component';
+import { ProductListingsCardComponent } from '../product-listings-card/product-listings-card.component';
+import productListingsData from '../../data/product-listings.data';
 
 @Component({
   selector: 'app-shop-new-arrivals',
-  imports: [],
+  imports: [
+    ProductFilterSidebarComponent,
+    ProductFilterButtonComponent,
+    ProductListingsCardComponent,
+  ],
   templateUrl: './shop-new-arrivals.component.html',
   styleUrl: './shop-new-arrivals.component.scss',
 })
-export class ShopNewArrivalsComponent {}
+export class ShopNewArrivalsComponent {
+  productListings = productListingsData;
+}
