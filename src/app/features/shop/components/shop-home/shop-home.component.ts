ebc3a7ad69436913e074e46ca0ca0145758d21d2
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-14 00:56:00 +0000

    implement shop home page listings

--- src/app/features/shop/components/shop-home/shop-home.component.ts
+++ src/app/features/shop/components/shop-home/shop-home.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import { ShopHomeProductsListingsComponent } from '../product-listings/product-listings.component';
+import { RouterLink } from '@angular/router';
 import { ProductCategoriesComponent } from '../../../../components/product-categories/product-categories.component';
+import { ProductFilterSidebarComponent } from '../product-filter-sidebar/product-filter-sidebar.component';
+import { ProductFilterButtonComponent } from '../product-filter-button/product-filter-button.component';
+import { ProductListingsCardComponent } from '../product-listings-card/product-listings-card.component';
+import productListingsData from '../../data/product-listings.data';
 
 @Component({
   selector: 'app-shop-home',
-  imports: [ShopHomeProductsListingsComponent, ProductCategoriesComponent],
+  imports: [
+    RouterLink,
+    ProductCategoriesComponent,
+    ProductFilterSidebarComponent,
+    ProductFilterButtonComponent,
+    ProductListingsCardComponent,
+  ],
   templateUrl: './shop-home.component.html',
   styleUrl: './shop-home.component.scss',
 })
-export class ShopHomeComponent {}
+export class ShopHomeComponent {
+  productListings = productListingsData;
+}
