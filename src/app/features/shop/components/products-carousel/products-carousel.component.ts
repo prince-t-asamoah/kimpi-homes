ee353c68f85d459dbf85f6b0c7f5a0963f5381bd
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-15 18:17:17 +0000

    crteate a product carousel container component

---
+++ src/app/features/shop/components/products-carousel/products-carousel.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-carousel',
+  imports: [],
+  templateUrl: './products-carousel.component.html',
+  styleUrl: './products-carousel.component.scss',
+})
+export class ProductsCarouselComponent {}
