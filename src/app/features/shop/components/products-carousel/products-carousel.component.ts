798d5df876e0c2f3b7671cf5e572a310ae5c5ca9
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-15 21:13:33 +0000

    update component with carousel button component and ng content

--- src/app/features/shop/components/products-carousel/products-carousel.component.ts
+++ src/app/features/shop/components/products-carousel/products-carousel.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CarouselButtonComponent } from '../../../../components/carousel-button/carousel-button.component';
 
 @Component({
   selector: 'app-products-carousel',
-  imports: [],
+  imports: [CarouselButtonComponent],
   templateUrl: './products-carousel.component.html',
   styleUrl: './products-carousel.component.scss',
 })
