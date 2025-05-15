55ed384ca4acb5d189ff78731c0ed50988947643
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-15 21:34:47 +0000

    use carousel button component

--- src/app/components/product-categories/product-categories.component.ts
+++ src/app/components/product-categories/product-categories.component.ts
@@ -1,9 +1,10 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
+import { CarouselButtonComponent } from '../carousel-button/carousel-button.component';
 
 @Component({
   selector: 'app-product-categories',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, CarouselButtonComponent],
   templateUrl: './product-categories.component.html',
   styleUrl: './product-categories.component.scss',
   encapsulation: ViewEncapsulation.None,
