34994dcaa00f3ba87d4fee11090ce4657081f790
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-18 16:57:19 +0000

    use carousel button component

--- src/app/features/home/components/hero/hero.component.ts
+++ src/app/features/home/components/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
+import { CarouselButtonComponent } from '../../../../components/carousel-button/carousel-button.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [LucideAngularModule, RouterModule],
+  imports: [LucideAngularModule, RouterModule, CarouselButtonComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
--- src/app/features/home/components/new-products/new-products.component.ts
+++ src/app/features/home/components/new-products/new-products.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
+import { CarouselButtonComponent } from '../../../../components/carousel-button/carousel-button.component';
 
 @Component({
   selector: 'app-new-products',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, CarouselButtonComponent],
   templateUrl: './new-products.component.html',
   styleUrl: './new-products.component.scss',
 })
