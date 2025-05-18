5b1cd8991a5c6cbe67c523e5b5abb38c39f86cfa
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-18 21:04:59 +0000

    import router link module

--- src/app/components/product-categories/product-categories.component.ts
+++ src/app/components/product-categories/product-categories.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { CarouselButtonComponent } from '../carousel-button/carousel-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-categories',
-  imports: [LucideAngularModule, CarouselButtonComponent],
+  imports: [LucideAngularModule, RouterLink, CarouselButtonComponent],
   templateUrl: './product-categories.component.html',
   styleUrl: './product-categories.component.scss',
   encapsulation: ViewEncapsulation.None,
