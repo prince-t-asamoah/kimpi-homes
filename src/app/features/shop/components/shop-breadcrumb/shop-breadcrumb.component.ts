6baafbb3025321d4f0684aaa758542999fbf6e69
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-18 19:13:37 +0000

    implement breadcrumb navigation

--- src/app/features/shop/components/shop-breadcrumb/shop-breadcrumb.component.ts
+++ src/app/features/shop/components/shop-breadcrumb/shop-breadcrumb.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shop-breadcrumb',
@@ -8,4 +9,27 @@ import { LucideAngularModule } from 'lucide-angular';
   templateUrl: './shop-breadcrumb.component.html',
   styleUrl: './shop-breadcrumb.component.scss',
 })
-export class ShopBreadcrumbComponent {}
+export class ShopBreadcrumbComponent implements OnDestroy {
+  private _router = inject(Router);
+  private _routerSubscription!: Subscription;
+  currentRoute = '';
+
+  constructor() {
+    this._routerSubscription = this._router.events
+      .pipe(
+        filter(
+          (event): event is NavigationEnd => event instanceof NavigationEnd
+        )
+      )
+      .subscribe(event => {
+        const lastRoute = event.url.split('/').pop();
+        this.currentRoute = lastRoute ? lastRoute : '';
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this._routerSubscription) {
+      this._routerSubscription.unsubscribe();
+    }
+  }
+}
