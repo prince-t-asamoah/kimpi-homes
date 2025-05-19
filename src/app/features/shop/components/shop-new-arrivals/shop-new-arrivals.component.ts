de2ef0d9639de8fba0fa7846b5992156f0ac2975
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-19 22:30:55 +0000

    import products pagination navigation bar

--- src/app/features/shop/components/shop-new-arrivals/shop-new-arrivals.component.ts
+++ src/app/features/shop/components/shop-new-arrivals/shop-new-arrivals.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ProductFilterSidebarComponent } from '../product-filter-sidebar/product-filter-sidebar.component';
 import { ProductFilterButtonComponent } from '../product-filter-button/product-filter-button.component';
 import { ProductListingsCardComponent } from '../product-listings-card/product-listings-card.component';
 import { ProductsCarouselComponent } from '../products-carousel/products-carousel.component';
 import productListingsData from '../../data/product-listings.data';
+import { ProductsPaginationButtonsComponent } from '../products-pagination-buttons/products-pagination-buttons.component';
 
 @Component({
   selector: 'app-shop-new-arrivals',
@@ -12,6 +14,8 @@ import productListingsData from '../../data/product-listings.data';
     ProductFilterButtonComponent,
     ProductListingsCardComponent,
     ProductsCarouselComponent,
+    RouterLink,
+    ProductsPaginationButtonsComponent,
   ],
   templateUrl: './shop-new-arrivals.component.html',
   styleUrl: './shop-new-arrivals.component.scss',
