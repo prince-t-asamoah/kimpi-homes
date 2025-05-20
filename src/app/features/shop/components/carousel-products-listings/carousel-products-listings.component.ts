2de00082bc6989838fe0f1f13393e32b5e86867c
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-20 14:29:27 +0000

    implement carousel scroll left and right

--- src/app/features/shop/components/carousel-products-listings/carousel-products-listings.component.ts
+++ src/app/features/shop/components/carousel-products-listings/carousel-products-listings.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CarouselButtonComponent } from '../../../../components/carousel-button/carousel-button.component';
 import { ProductListingsCardComponent } from '../product-listings-card/product-listings-card.component';
@@ -15,4 +15,13 @@ export class CarouselProductsListingsComponent {
   @Input({ required: true }) title = 'No title';
   @Input({ required: true }) description = 'No description';
   @Input({ required: true }) viewAllLink = '';
+  @ViewChild('carousel', { static: false }) carousel!: ElementRef<HTMLElement>;
+
+  scrollLeft(): void {
+    this.carousel.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+
+  scrollRight(): void {
+    this.carousel.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
 }
