8a0f2ca06d65dd2fc9f70862cb11c7c9bc51297b
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-20 13:20:07 +0000

    import recommended product listings component

--- src/app/features/shop/components/shop-top-sellers/shop-top-sellers.component.ts
+++ src/app/features/shop/components/shop-top-sellers/shop-top-sellers.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { ProductFilterButtonComponent } from '../product-filter-button/product-filter-button.component';
+import { ProductFilterSidebarComponent } from '../product-filter-sidebar/product-filter-sidebar.component';
+import { TopSellersListingsComponent } from './components/top-sellers-listings/top-sellers-listings.component';
+import { RecommendedProductsListingsComponent } from '../recommended-products-listings/recommended-products-listings.component';
+import productListingsData from '../../data/product-listings.data';
 
 @Component({
   selector: 'app-shop-top-sellers',
-  imports: [],
+  imports: [
+    ProductFilterButtonComponent,
+    ProductFilterSidebarComponent,
+    TopSellersListingsComponent,
+    RecommendedProductsListingsComponent,
+  ],
   templateUrl: './shop-top-sellers.component.html',
   styleUrl: './shop-top-sellers.component.scss',
 })
-export class ShopTopSellersComponent {}
+export class ShopTopSellersComponent {
+  public readonly productListings = productListingsData;
+  currantPageNumber = 1;
+  totalPages = 10;
+}
