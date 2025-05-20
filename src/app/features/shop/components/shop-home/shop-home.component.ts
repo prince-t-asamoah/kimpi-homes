597797bf439cd83baa5b3c420ea1fa9023ce626b
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-20 14:45:38 +0000

    update the page to use the product carousel component

--- src/app/features/shop/components/shop-home/shop-home.component.ts
+++ src/app/features/shop/components/shop-home/shop-home.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { ProductCategoriesComponent } from '../../../../components/product-categories/product-categories.component';
 import { ProductFilterSidebarComponent } from '../product-filter-sidebar/product-filter-sidebar.component';
 import { ProductFilterButtonComponent } from '../product-filter-button/product-filter-button.component';
-import { ProductListingsCardComponent } from '../product-listings-card/product-listings-card.component';
-import { ProductsCarouselComponent } from '../products-carousel/products-carousel.component';
+import { CarouselProductsListingsComponent } from '../carousel-products-listings/carousel-products-listings.component';
 import productListingsData from '../../data/product-listings.data';
 
 @Component({
   selector: 'app-shop-home',
   imports: [
-    RouterLink,
     ProductCategoriesComponent,
     ProductFilterSidebarComponent,
     ProductFilterButtonComponent,
-    ProductListingsCardComponent,
-    ProductsCarouselComponent,
+    CarouselProductsListingsComponent,
   ],
   templateUrl: './shop-home.component.html',
   styleUrl: './shop-home.component.scss',
