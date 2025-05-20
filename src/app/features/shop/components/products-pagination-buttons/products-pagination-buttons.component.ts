a5ce6c5dab3f48f3105a9425c7da5139d1f2a052
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-20 00:12:37 +0000

    implement product pagination navigation

--- src/app/features/shop/components/products-pagination-buttons/products-pagination-buttons.component.ts
+++ src/app/features/shop/components/products-pagination-buttons/products-pagination-buttons.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-products-pagination-buttons',
-  imports: [],
+  imports: [LucideAngularModule, RouterLinkActive, RouterLink],
   templateUrl: './products-pagination-buttons.component.html',
   styleUrl: './products-pagination-buttons.component.scss',
 })
-export class ProductsPaginationButtonsComponent {}
+export class ProductsPaginationButtonsComponent {
+  @Input({ required: true }) currentPageNumber = 1;
+  @Input({ required: true }) totalPages = 10;
+  @Input({ required: true }) currentPageRoute = '';
+
+  pageCounter(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+}
