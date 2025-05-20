4669ad2233acbaed7c1c9e9de5fcaf8f68d56ac4
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-20 13:17:46 +0000

    implement recommended products listings

--- src/app/features/shop/components/recommended-products-listings/recommended-products-listings.component.ts
+++ src/app/features/shop/components/recommended-products-listings/recommended-products-listings.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ProductsCarouselComponent } from '../products-carousel/products-carousel.component';
+import { ProductListingsCardComponent } from '../product-listings-card/product-listings-card.component';
+import { Product } from './../../model/product-listings.model';
 
 @Component({
   selector: 'app-recommended-products-listings',
-  imports: [],
+  imports: [
+    RouterLink,
+    ProductsCarouselComponent,
+    ProductListingsCardComponent,
+  ],
   templateUrl: './recommended-products-listings.component.html',
   styleUrl: './recommended-products-listings.component.scss',
 })
-export class RecommendedProductsListingsComponent {}
+export class RecommendedProductsListingsComponent {
+  @Input({ required: true }) productListings: Product[] = [];
+}
