37b2992020163869edd4ebac9d7b5b3485796d36
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-20 13:57:17 +0000

    use project carousel component

--- src/app/features/shop/components/carousel-products-listings/carousel-products-listings.component.ts
+++ src/app/features/shop/components/carousel-products-listings/carousel-products-listings.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CarouselButtonComponent } from '../../../../components/carousel-button/carousel-button.component';
+import { ProductListingsCardComponent } from '../product-listings-card/product-listings-card.component';
+import { Product } from '../../model/product-listings.model';
 
 @Component({
   selector: 'app-carousel-products-listings',
-  imports: [],
+  imports: [RouterLink, CarouselButtonComponent, ProductListingsCardComponent],
   templateUrl: './carousel-products-listings.component.html',
   styleUrl: './carousel-products-listings.component.scss',
 })
-export class CarouselProductsListingsComponent {}
+export class CarouselProductsListingsComponent {
+  @Input({ required: true }) productListings: Product[] = [];
+  @Input({ required: true }) title = 'No title';
+  @Input({ required: true }) description = 'No description';
+  @Input({ required: true }) viewAllLink = '';
+}
