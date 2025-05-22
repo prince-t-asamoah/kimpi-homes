fa8398489444adf9ddd3daf9cd6c9e4e82a1d920
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-22 21:53:44 +0000

    implement a get api service for apis request

--- src/app/services/api-service/api-service.service.ts
+++ src/app/services/api-service/api-service.service.ts
@@ -1,6 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ApiServiceService {}
+export class ApiServiceService {
+  private apiUrl = '';
+
+  constructor(private _httpClient: HttpClient) {}
+
+  get<T>(endpoint: string): Observable<T> {
+    return this._httpClient.get<T>(`${this.apiUrl}/${endpoint}`);
+  }
+}
