56664f40ac815feeb4c18065af1ffbc4b24bb9f3
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-22 22:06:19 +0000

    add interface for get product listings api response and update product interface

--- src/app/features/shop/model/product-listings.model.ts
+++ src/app/features/shop/model/product-listings.model.ts
@@ -1,16 +1,54 @@
+import { GetApiResponse } from '../../../model/api-response';
+
 export interface Product {
   id: string;
   imageUrl: string;
   name: string;
-  currentPrice: string;
-  previousPrice: string;
+  price: {
+    original: string;
+    discounted: string;
+    discountPercent: number;
+    currency: string;
+  };
+  originalPrice: string;
+  discountedPrice: string;
   discount?: string;
-  ratings: number;
+  rating: number;
   reviews: unknown[];
+  reviewsCount: number;
+  discountPercentage?: number;
 }
 
 export interface ProductFilter {
   id: string;
   name: string;
   filterBy: { id: string; name?: string; value?: unknown }[];
+}
+
+export interface GetProductListingsAPIResponse
+  extends GetApiResponse<ProductListings> {
+  status: string;
+  timestamp: string;
+  data: {
+    category: {
+      id: string;
+      name: string;
+      imageUrl: string;
+    };
+    topSellers: Product[];
+    newArrivals: Product[];
+    flashSales: Product[];
+    recommended: Product[];
+  };
+}
+export interface ProductListings {
+  category: {
+    id: string;
+    name: string;
+    imageUrl: string;
+  };
+  topSellers: Product[];
+  newArrivals: Product[];
+  flashSales: Product[];
+  recommended: Product[];
 }
