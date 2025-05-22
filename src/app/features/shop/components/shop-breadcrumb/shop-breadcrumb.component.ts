666286b72b02e1f8c1a1cb80594ea6f2d3b93776
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-22 09:54:20 +0000

    refactor the breadcrumb implementation using input props

--- src/app/features/shop/components/shop-breadcrumb/shop-breadcrumb.component.ts
+++ src/app/features/shop/components/shop-breadcrumb/shop-breadcrumb.component.ts
@@ -1,7 +1,6 @@
-import { Component, inject, OnDestroy } from '@angular/core';
-import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
-import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shop-breadcrumb',
@@ -9,27 +8,10 @@ import { filter, Subscription } from 'rxjs';
   templateUrl: './shop-breadcrumb.component.html',
   styleUrl: './shop-breadcrumb.component.scss',
 })
-export class ShopBreadcrumbComponent implements OnDestroy {
-  private _router = inject(Router);
-  private _routerSubscription!: Subscription;
-  currentRoute = '';
-
-  constructor() {
-    this._routerSubscription = this._router.events
-      .pipe(
-        filter(
-          (event): event is NavigationEnd => event instanceof NavigationEnd
-        )
-      )
-      .subscribe(event => {
-        const lastRoute = event.url.split('/').pop();
-        this.currentRoute = lastRoute ? lastRoute : '';
-      });
-  }
-
-  ngOnDestroy(): void {
-    if (this._routerSubscription) {
-      this._routerSubscription.unsubscribe();
-    }
-  }
+export class ShopBreadcrumbComponent {
+  @Input({ required: true }) currentRoute:
+    | 'home'
+    | 'new-arrivals'
+    | 'top-sellers'
+    | 'flash-sales' = 'home';
 }
