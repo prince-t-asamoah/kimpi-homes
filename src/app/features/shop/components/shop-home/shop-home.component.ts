f66de4769fea23ba802379b852dd050d802e3b99
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-23 11:42:10 +0000

    implement getting product listings data

--- src/app/features/shop/components/shop-home/shop-home.component.ts
+++ src/app/features/shop/components/shop-home/shop-home.component.ts
@@ -1,25 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { ProductCategoriesComponent } from '../../../../components/product-categories/product-categories.component';
 import { ProductFilterSidebarComponent } from '../product-filter-sidebar/product-filter-sidebar.component';
 import { ProductFilterButtonComponent } from '../product-filter-button/product-filter-button.component';
 import { CarouselProductsListingsComponent } from '../carousel-products-listings/carousel-products-listings.component';
-import productListingsData from '../../data/product-listings.data';
 import { ShopBreadcrumbComponent } from '../shop-breadcrumb/shop-breadcrumb.component';
+import { ProductListingsService } from '../../services/product-listings/product-listings.service';
+import { ProductListings } from '../../model/product-listings.model';
 
 @Component({
   selector: 'app-shop-home',
   imports: [
+    LucideAngularModule,
     ProductCategoriesComponent,
     ProductFilterSidebarComponent,
     ProductFilterButtonComponent,
     CarouselProductsListingsComponent,
-    LucideAngularModule,
     ShopBreadcrumbComponent,
   ],
   templateUrl: './shop-home.component.html',
   styleUrl: './shop-home.component.scss',
 })
 export class ShopHomeComponent {
-  productListings = productListingsData;
+  productListings: ProductListings | null = null;
+  productListingsService = inject(ProductListingsService);
+
+  constructor() {
+    this.getAllProductListings();
+  }
+
+  getAllProductListings(): void {
+    this.productListingsService.getAllProductListings().subscribe({
+      next: response => {
+        if (response.data) {
+          this.productListings = response.data;
+        }
+      },
+    });
+  }
 }
