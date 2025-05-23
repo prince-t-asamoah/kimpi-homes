e65e1a528d7bd637a017dd3c21741723c2857a0d
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-23 14:22:14 +0000

    implement fetching product listings

--- src/app/features/shop/components/shop-top-sellers/shop-top-sellers.component.ts
+++ src/app/features/shop/components/shop-top-sellers/shop-top-sellers.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProductFilterButtonComponent } from '../product-filter-button/product-filter-button.component';
 import { ProductFilterSidebarComponent } from '../product-filter-sidebar/product-filter-sidebar.component';
-import productListingsData from '../../data/product-listings.data';
 import { CarouselProductsListingsComponent } from '../carousel-products-listings/carousel-products-listings.component';
 import { PaginatedProductListingsComponent } from '../paginated-product-listings/paginated-product-listings.component';
 import { ShopBreadcrumbComponent } from '../shop-breadcrumb/shop-breadcrumb.component';
+import { Product } from '../../model/product-listings.model';
+import { ProductListingsService } from '../../services/product-listings/product-listings.service';
 
 @Component({
   selector: 'app-shop-top-sellers',
@@ -19,8 +20,30 @@ import { ShopBreadcrumbComponent } from '../shop-breadcrumb/shop-breadcrumb.comp
   styleUrl: './shop-top-sellers.component.scss',
 })
 export class ShopTopSellersComponent {
-  public readonly productListings = productListingsData;
+  public readonly productListings: Product[] = [];
+  recommendedListings: Product[] = [];
+  topSellersListings: Product[] = [];
   currantPageNumber = 1;
   totalPages = 10;
   totalProducts = 1500;
+
+  constructor(private _productListingsService: ProductListingsService) {
+    this.getProductListings();
+  }
+
+  getProductListings(): void {
+    this.recommendedListings = [];
+    this.topSellersListings = [];
+    this._productListingsService.getAllProductListings().subscribe({
+      next: response => {
+        if (response.data) {
+          this.recommendedListings = response.data.recommended;
+          this.topSellersListings = response.data.topSellers;
+        }
+      },
+      error: error => {
+        console.error('Error fetching product listings:', error);
+      },
+    });
+  }
 }
