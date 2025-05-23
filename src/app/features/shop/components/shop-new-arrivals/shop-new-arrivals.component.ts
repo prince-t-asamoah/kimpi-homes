d05a1418581e4cf014df966f7dcecbb39cd6b900
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-23 12:00:53 +0000

    implement get recommended and new arrivals listings

--- src/app/features/shop/components/shop-new-arrivals/shop-new-arrivals.component.ts
+++ src/app/features/shop/components/shop-new-arrivals/shop-new-arrivals.component.ts
@@ -4,8 +4,9 @@ import { ProductFilterSidebarComponent } from '../product-filter-sidebar/product
 import { ProductFilterButtonComponent } from '../product-filter-button/product-filter-button.component';
 import { CarouselProductsListingsComponent } from '../carousel-products-listings/carousel-products-listings.component';
 import { PaginatedProductListingsComponent } from '../paginated-product-listings/paginated-product-listings.component';
-import productListingsData from '../../data/product-listings.data';
 import { ShopBreadcrumbComponent } from '../shop-breadcrumb/shop-breadcrumb.component';
+import { Product } from '../../model/product-listings.model';
+import { ProductListingsService } from '../../services/product-listings/product-listings.service';
 
 @Component({
   selector: 'app-shop-new-arrivals',
@@ -20,15 +21,38 @@ import { ShopBreadcrumbComponent } from '../shop-breadcrumb/shop-breadcrumb.comp
   styleUrl: './shop-new-arrivals.component.scss',
 })
 export class ShopNewArrivalsComponent implements OnInit {
-  public readonly productListings = productListingsData;
-  currantPageNumber = 1;
+  productListings: Product[] = [];
+  recommendedListings: Product[] = [];
+  newArrivalsListings: Product[] = [];
+  currentPageNumber = 1;
   totalPages = 10;
   totalProducts = 1500;
 
-  constructor(private _router: ActivatedRoute) {}
+  constructor(
+    private _router: ActivatedRoute,
+    private _productListingsService: ProductListingsService
+  ) {}
 
   ngOnInit(): void {
     const pageNumber = this._router.snapshot.paramMap.get('pageNumber') ?? 1;
-    this.currantPageNumber = Number(pageNumber);
+    this.currentPageNumber = Number(pageNumber);
+
+    this.getProductListings();
+  }
+
+  getProductListings(): void {
+    this.recommendedListings = [];
+    this.newArrivalsListings = [];
+    this._productListingsService.getAllProductListings().subscribe({
+      next: response => {
+        if (response.data) {
+          this.recommendedListings = response.data.recommended;
+          this.newArrivalsListings = response.data.newArrivals;
+        }
+      },
+      error: error => {
+        console.error('Error fetching product listings:', error);
+      },
+    });
   }
 }
