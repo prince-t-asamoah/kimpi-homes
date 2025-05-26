e2be0dedd1993ff83d562eb15559cfc6d7fc7b89
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-26 18:23:13 +0000

    create a customer enquiry form component

---
+++ src/app/components/customer-enquiry/customer-enquiry.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-enquiry',
+  imports: [],
+  templateUrl: './customer-enquiry.component.html',
+  styleUrl: './customer-enquiry.component.scss',
+})
+export class CustomerEnquiryComponent {}
