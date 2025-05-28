500d22347bdb2a1cf78cf191e000bb8dbec2ccb5
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-28 20:39:56 +0000

    implement customer account page layout and sidebar navigation

--- src/app/features/customer/customer.component.ts
+++ src/app/features/customer/customer.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { LanguageBarComponent } from '../../components/language-bar/language-bar.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    LucideAngularModule,
+    LanguageBarComponent,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.scss',
 })
