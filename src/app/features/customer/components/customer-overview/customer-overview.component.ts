94d348ae05f0ef9f0e5f744997ea7ab0c30cfe88
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-28 15:01:48 +0000

    implement overview page header ui

--- src/app/features/customer/components/customer-overview/customer-overview.component.ts
+++ src/app/features/customer/components/customer-overview/customer-overview.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-customer-overview',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './customer-overview.component.html',
   styleUrl: './customer-overview.component.scss',
 })
