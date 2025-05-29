ba320b2bb01b75df1360a19ae5618f435639d2f1
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-29 15:03:29 +0000

    implement customer overview ui and service

--- src/app/features/customer/components/customer-overview/customer-overview.component.ts
+++ src/app/features/customer/components/customer-overview/customer-overview.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 import { LucideAngularModule } from 'lucide-angular';
+import { CustomerService } from '../../services/customer.service';
+import { CustomerOrder } from '../../model/overview';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-customer-overview',
-  imports: [LucideAngularModule],
+  imports: [RouterLink, LucideAngularModule, DatePipe],
   templateUrl: './customer-overview.component.html',
   styleUrl: './customer-overview.component.scss',
 })
-export class CustomerOverviewComponent {}
+export class CustomerOverviewComponent implements OnInit, OnDestroy {
+  private _destroy$ = new Subject<void>();
+  totalOrders = 0;
+  totalShipment = 0;
+  totalReturns = 0;
+  recentOrders: CustomerOrder[] = [];
+
+  constructor(private _customerService: CustomerService) {}
+
+  ngOnInit() {
+    this.getOverviewData();
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+
+  getOverviewData(): void {
+    this._customerService
+      .getOverviewData()
+      .pipe(takeUntil(this._destroy$))
+      .subscribe({
+        next: response => {
+          if (response.data) {
+            this.totalOrders = response.data.statistics.totalOrders;
+            this.totalShipment = response.data.statistics.totalShipment;
+            this.totalReturns = response.data.statistics.totalReturns;
+            this.recentOrders = response.data.recentOrders;
+          }
+        },
+        error: error => {
+          console.error('Error fetching overview data:', error);
+        },
+      });
+  }
+}
--- src/app/features/customer/services/customer.service.ts
+++ src/app/features/customer/services/customer.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiServiceService } from '../../../services/api-service/api-service.service';
+import { GetCusomerOverviewAPIResponse } from '../model/overview';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CustomerService {}
+export class CustomerService {
+  constructor(private _apiClientService: ApiServiceService) {}
+
+  getOverviewData(): Observable<GetCusomerOverviewAPIResponse> {
+    return this._apiClientService.get('assets/data/customer/overview.json');
+  }
+}
