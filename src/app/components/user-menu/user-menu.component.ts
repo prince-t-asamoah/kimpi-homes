f792a46a139c380bb41ef01bba8fcb4b6c077ed1
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-30 09:08:02 +0000

    create user menu compnent

---
+++ src/app/components/user-menu/user-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-menu',
+  imports: [],
+  templateUrl: './user-menu.component.html',
+  styleUrl: './user-menu.component.scss',
+})
+export class UserMenuComponent {}
