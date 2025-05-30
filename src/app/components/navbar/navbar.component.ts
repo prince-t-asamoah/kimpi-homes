b7dbec4464a51ae65165105401012548945cf3a4
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-30 13:27:50 +0000

    implement user menu component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,17 +2,18 @@ import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
+import { UserMenuComponent } from '../user-menu/user-menu.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgClass, LucideAngularModule, RouterModule],
+  imports: [NgClass, LucideAngularModule, RouterModule, UserMenuComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   showMainNavigation = false;
   showSearchProductBar = false;
-  isAuthenticated = false;
+  isAuthenticated = true;
 
   toggleMainNavigation(): void {
     this.showMainNavigation = !this.showMainNavigation;
--- src/app/components/user-menu/user-menu.component.ts
+++ src/app/components/user-menu/user-menu.component.ts
@@ -1,9 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-user-menu',
-  imports: [],
+  imports: [LucideAngularModule, NgClass],
   templateUrl: './user-menu.component.html',
   styleUrl: './user-menu.component.scss',
 })
-export class UserMenuComponent {}
+export class UserMenuComponent {
+  showUserMenu = false;
+
+  toggleUserMenu(): void {
+    this.showUserMenu = !this.showUserMenu;
+  }
+}
