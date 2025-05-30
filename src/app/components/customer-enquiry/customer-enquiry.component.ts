896870391aec339cb883a2bc95aedb94237678ca
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-30 13:59:20 +0000

    use  form input component

--- src/app/components/customer-enquiry/customer-enquiry.component.ts
+++ src/app/components/customer-enquiry/customer-enquiry.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
+import { FormInputComponent } from '../form-input/form-input.component';
 
 @Component({
   selector: 'app-customer-enquiry',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, FormInputComponent],
   templateUrl: './customer-enquiry.component.html',
   styleUrl: './customer-enquiry.component.scss',
 })
