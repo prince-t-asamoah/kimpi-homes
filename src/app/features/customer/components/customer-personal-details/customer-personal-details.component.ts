744431194f875b167c6a10956844116386368a14
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-05-31 13:23:14 +0000

    implement profile form ui

--- src/app/features/customer/components/customer-personal-details/customer-personal-details.component.ts
+++ src/app/features/customer/components/customer-personal-details/customer-personal-details.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { LucideAngularModule } from 'lucide-angular';
+import { FormInputComponent } from '../../../../components/form-input/form-input.component';
+import telephoneCountryCodes from '../../data/country-telephone-code.data';
 
 @Component({
   selector: 'app-customer-personal-details',
-  imports: [],
+  imports: [FormInputComponent, LucideAngularModule, NgClass],
   templateUrl: './customer-personal-details.component.html',
   styleUrl: './customer-personal-details.component.scss',
 })
-export class CustomerPersonalDetailsComponent {}
+export class CustomerPersonalDetailsComponent {
+  showTelephoneDropdown = false;
+  selectedCountryCode = telephoneCountryCodes[0];
+  countryCodes = telephoneCountryCodes;
+  @ViewChild('telephoneNumberContainer') telephoneNumberContainer!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent): void {
+    if (!this.telephoneNumberContainer?.nativeElement.contains(event.target)) {
+      this.showTelephoneDropdown = false;
+    }
+  }
+
+  toggleTelephoneDropdown(): void {
+    this.showTelephoneDropdown = !this.showTelephoneDropdown;
+  }
+
+  selectCountryCode(code: string): void {
+    if (this.selectedCountryCode === code) return;
+    this.selectedCountryCode = code;
+    this.toggleTelephoneDropdown();
+  }
+}
