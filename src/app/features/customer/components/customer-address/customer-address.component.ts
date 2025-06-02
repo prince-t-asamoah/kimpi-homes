f9770aa250118f26c953d013fffa2c878bda329a
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-02 15:27:42 +0000

    create a customer address page component

---
+++ src/app/features/customer/components/customer-address/customer-address.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-address',
+  imports: [],
+  templateUrl: './customer-address.component.html',
+  styleUrl: './customer-address.component.scss',
+})
+export class CustomerAddressComponent {}
