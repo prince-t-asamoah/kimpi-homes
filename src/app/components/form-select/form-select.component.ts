a0b045b382559e9f6b8d6f626eddadb7b8f0523b
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-02 17:23:51 +0000

    create form select component

---
+++ src/app/components/form-select/form-select.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-select',
+  imports: [],
+  templateUrl: './form-select.component.html',
+  styleUrl: './form-select.component.scss',
+})
+export class FormSelectComponent {}
