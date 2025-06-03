f1ca63db0e14a3b8ebfb38da38725f208eebb93b
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-03 11:47:57 +0000

    use form checkbox component

--- src/app/features/customer/components/customer-address/customer-address.component.ts
+++ src/app/features/customer/components/customer-address/customer-address.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormSelectComponent } from '../../../../components/form-select/form-select.component';
 import { FormInputComponent } from '../../../../components/form-input/form-input.component';
+import { FormCheckboxComponent } from '../../../../components/form-checkbox/form-checkbox.component';
 import countries from '../../data/country-telephone-code.data';
 
 @Component({
   selector: 'app-customer-address',
-  imports: [FormSelectComponent, FormInputComponent],
+  imports: [FormSelectComponent, FormInputComponent, FormCheckboxComponent],
   templateUrl: './customer-address.component.html',
   styleUrls: [
     './customer-address.component.scss',
