d694e090f99dea45fee8c944cb6cdec8099c4a27
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-03 11:43:37 +0000

    implement form checkbox component

--- src/app/components/form-checkbox/form-checkbox.component.ts
+++ src/app/components/form-checkbox/form-checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form-checkbox',
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   templateUrl: './form-checkbox.component.html',
   styleUrl: './form-checkbox.component.scss',
 })
-export class FormCheckboxComponent {}
+export class FormCheckboxComponent {
+  @Input() id = '';
+  @Input() name = '';
+  @Input() value = '';
+  @Input() disabled = false;
+  @Input() checked = false;
+  @Input() type: 'checkbox' | 'radio' = 'checkbox';
+}
