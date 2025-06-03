c0ddaa3d8337d4505a89dc6eb156a739b184692a
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-03 11:13:42 +0000

    create form checkbox component

---
+++ src/app/components/form-checkbox/form-checkbox.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-checkbox',
+  imports: [],
+  templateUrl: './form-checkbox.component.html',
+  styleUrl: './form-checkbox.component.scss',
+})
+export class FormCheckboxComponent {}
