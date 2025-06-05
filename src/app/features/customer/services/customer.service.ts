f87bf3df910ef86ebcaec97d2268267d5d138f74
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-05 14:58:37 +0000

    implement get customer messages service

--- src/app/features/customer/services/customer.service.ts
+++ src/app/features/customer/services/customer.service.ts
@@ -3,6 +3,7 @@ import { delay, Observable } from 'rxjs';
 import { ApiServiceService } from '../../../services/api-service/api-service.service';
 import { GetCustomerOverviewAPIResponse } from '../model/overview';
 import { GetCustomerOrderHistoryAPIResponse } from '../model/order-history.model';
+import { GetCustomerMessagesAPIResponse } from '../model/messages.model';
 
 @Injectable({
   providedIn: 'root',
@@ -19,6 +20,12 @@ export class CustomerService {
       .get<GetCustomerOrderHistoryAPIResponse>(
         'assets/data/customer/order-history.json'
       )
+      .pipe(delay(1000));
+  }
+
+  getMessages(): Observable<GetCustomerMessagesAPIResponse> {
+    return this._apiClientService
+      .get<GetCustomerMessagesAPIResponse>('assets/data/customer/messages.json')
       .pipe(delay(1000));
   }
 }
