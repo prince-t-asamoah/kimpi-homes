88a53e18a0ee3d99b0e6c6ea9dd03e6c01e69f7e
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-05 15:57:36 +0000

    implement loading customer messages

--- src/app/features/customer/components/customer-messages/customer-messages.component.ts
+++ src/app/features/customer/components/customer-messages/customer-messages.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MessagesListComponent } from './components/messages-list/messages-list.component';
+import { MessagesViewerComponent } from './components/messages-viewer/messages-viewer.component';
+import { CustomerService } from '../../services/customer.service';
+import { CustomerMessage } from '../../model/messages.model';
+import { SpinningLoaderComponent } from '../../../../components/spinning-loader/spinning-loader.component';
+import { LoadingErrorComponent } from '../../../../components/loading-error/loading-error.component';
 
 @Component({
   selector: 'app-customer-messages',
-  imports: [],
+  imports: [
+    MessagesListComponent,
+    MessagesViewerComponent,
+    SpinningLoaderComponent,
+    LoadingErrorComponent,
+  ],
   templateUrl: './customer-messages.component.html',
   styleUrl: './customer-messages.component.scss',
 })
-export class CustomerMessagesComponent {}
+export class CustomerMessagesComponent implements OnInit {
+  private _customerService = inject(CustomerService);
+  messages: CustomerMessage[] = [];
+  isLoading = true;
+  hasLoadingError = false;
+  selectedMessage: CustomerMessage | null = null;
+
+  ngOnInit(): void {
+    this.getMessages();
+  }
+
+  getMessages(): void {
+    this._customerService.getMessages().subscribe({
+      next: response => {
+        if (response.data) {
+          this.messages = response.data;
+        }
+        this.isLoading = false;
+      },
+      error: () => {
+        this.hasLoadingError = true;
+        this.isLoading = false;
+      },
+    });
+  }
+
+  selectMessage(message: CustomerMessage): void {
+    this.selectedMessage = message;
+  }
+
+  closeMessageViewer(): void {
+    this.selectedMessage = null;
+  }
+}
