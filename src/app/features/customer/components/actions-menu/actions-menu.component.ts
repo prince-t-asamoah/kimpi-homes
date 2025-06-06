4480413999edcf82559d6e3d3586f14e84e1254d
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-06 15:51:04 +0000

    create customer actions menu component

---
+++ src/app/features/customer/components/actions-menu/actions-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actions-menu',
+  imports: [],
+  templateUrl: './actions-menu.component.html',
+  styleUrl: './actions-menu.component.scss',
+})
+export class ActionsMenuComponent {}
