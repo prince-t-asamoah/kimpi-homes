6cc6fe9be588cf28f57664ce7939ce8bcbb181be
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-09 23:33:30 +0000

    create delete account dialog component

---
+++ src/app/features/customer/customer-account-settings/components/delete-account-dialog/delete-account-dialog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-account-dialog',
+  imports: [],
+  templateUrl: './delete-account-dialog.component.html',
+  styleUrl: './delete-account-dialog.component.scss',
+})
+export class DeleteAccountDialogComponent {}
