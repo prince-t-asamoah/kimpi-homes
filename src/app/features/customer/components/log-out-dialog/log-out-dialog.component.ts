de00cd51dbacf5975d6c50cc1509fa51ce0f6f6e
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-10 17:39:28 +0000

    implement logout dialog ui

--- src/app/features/customer/components/log-out-dialog/log-out-dialog.component.ts
+++ src/app/features/customer/components/log-out-dialog/log-out-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-log-out-dialog',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './log-out-dialog.component.html',
   styleUrl: './log-out-dialog.component.scss',
 })
-export class LogOutDialogComponent {}
+export class LogOutDialogComponent {
+  @Output() cancelDialog = new EventEmitter<void>();
+}
