5f3b23b02c7e8b599944ab1180a1569a47a6ffe8
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-10 14:02:33 +0000

    create dialog component

---
+++ src/app/features/customer/components/log-out-dialog/log-out-dialog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-out-dialog',
+  imports: [],
+  templateUrl: './log-out-dialog.component.html',
+  styleUrl: './log-out-dialog.component.scss',
+})
+export class LogOutDialogComponent {}
