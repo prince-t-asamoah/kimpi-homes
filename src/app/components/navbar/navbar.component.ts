c966feaeb4f1d817584bef46f4c46ee47aa51b4f
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-11 16:04:07 +0000

    update navbar search input and buttons ui implementation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,27 +1,57 @@
-import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 import { UserMenuComponent } from '../user-menu/user-menu.component';
+import { SearchInputComponent } from '../search-input/search-input.component';
+import { ProductSearchSuggestionsComponent } from '../product-search-suggestions/product-search-suggestions.component';
+import { ProductSearchResultsDropdownComponent } from '../product-search-results-dropdown/product-search-results-dropdown.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgClass, LucideAngularModule, RouterModule, UserMenuComponent],
+  imports: [
+    LucideAngularModule,
+    RouterModule,
+    UserMenuComponent,
+    SearchInputComponent,
+    ProductSearchSuggestionsComponent,
+    ProductSearchResultsDropdownComponent,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   showMainNavigation = false;
   showSearchProductBar = false;
+  showSearchResultsList = false;
   isAuthenticated = true;
 
   toggleMainNavigation(): void {
     this.showMainNavigation = !this.showMainNavigation;
-    this.showSearchProductBar = false;
   }
 
   toggleSearchProductBar(): void {
     this.showSearchProductBar = !this.showSearchProductBar;
     this.showMainNavigation = false;
+  }
+
+  toggleSearchResults(): void {
+    this.showSearchResultsList = !this.showSearchResultsList;
+  }
+
+  searchInputChanged(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.value.length > 0) {
+      this.showSearchResultsList = true;
+    } else {
+      this.showSearchResultsList = false;
+    }
+  }
+
+  searchInputFocused(): void {
+    this.showSearchProductBar = true;
+  }
+
+  searchInputBlurred(): void {
+    this.showSearchProductBar = false;
   }
 }
