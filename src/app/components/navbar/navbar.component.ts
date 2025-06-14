9c5145309281bcda4b54944b2da35bb5590f919b
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-14 19:59:19 +0000

    implement search product input

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgClass } from '@angular/common';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 import { LucideAngularModule } from 'lucide-angular';
 import { UserMenuComponent } from '../user-menu/user-menu.component';
 import { SearchInputComponent } from '../search-input/search-input.component';
 import { ProductSearchSuggestionsComponent } from '../product-search-suggestions/product-search-suggestions.component';
 import { ProductSearchResultsDropdownComponent } from '../product-search-results-dropdown/product-search-results-dropdown.component';
+import { ProductListingsService } from '../../features/shop/services/product-listings/product-listings.service';
+import { SearchItem } from '../../model/search.model';
 
 @Component({
   selector: 'app-navbar',
@@ -21,12 +24,34 @@ import { ProductSearchResultsDropdownComponent } from '../product-search-results
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  private readonly _productListingsService = inject(ProductListingsService);
+  private _searchProductEvent = new Subject<string>();
+  private _searchProductEventSubscription = new Subject<void>();
+
   showMainNavigation = false;
   showSearchProductBar = false;
   showSearchResultsList = false;
   isAuthenticated = true;
   cartCount = 0;
+  searchResults: SearchItem[] = [];
+
+  ngOnInit(): void {
+    this._searchProductEvent
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        takeUntil(this._searchProductEventSubscription)
+      )
+      .subscribe(() => {
+        this._getProductSearchByTerm();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._searchProductEventSubscription.next();
+    this._searchProductEventSubscription.complete();
+  }
 
   toggleMainNavigation(): void {
     this.showMainNavigation = !this.showMainNavigation;
@@ -45,8 +70,10 @@ export class NavbarComponent {
     const inputElement = event.target as HTMLInputElement;
     if (inputElement.value.length > 0) {
       this.showSearchResultsList = true;
+      this._searchProductEvent.next(inputElement.value);
     } else {
       this.showSearchResultsList = false;
+      this.searchResults = [];
     }
   }
 
@@ -56,5 +83,16 @@ export class NavbarComponent {
 
   searchInputBlurred(): void {
     this.showSearchProductBar = false;
+  }
+
+  private _getProductSearchByTerm() {
+    return this._productListingsService.searchProductsByQuery().subscribe({
+      next: response => {
+        this.searchResults = response.data.suggestions;
+      },
+      error: error => {
+        console.error('Error fetching search results:', error);
+      },
+    });
   }
 }
