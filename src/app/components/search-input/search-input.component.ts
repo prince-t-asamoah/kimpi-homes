2c7ed4807c53c53c06d62897b74fbfbb57633932
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-15 12:22:49 +0000

    implement clear search input

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,17 +1,41 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-search-input',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, ReactiveFormsModule],
   templateUrl: './search-input.component.html',
   styleUrl: './search-input.component.scss',
 })
 export class SearchInputComponent {
-  @Input() placeholder = '';
-  @Input() value = '';
+  value = '';
   @Input() id = '';
+  @Input() placeholder = '';
   @Output() focusEvent = new EventEmitter<FocusEvent>();
   @Output() inputEvent = new EventEmitter<Event>();
   @Output() blurEvent = new EventEmitter<Event>();
+  @ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
+
+  clearSearchInput(): void {
+    this.value = '';
+    this.searchInput.nativeElement.value = '';
+    this.searchInput.nativeElement.dispatchEvent(
+      new Event('input', { bubbles: true })
+    );
+    this.searchInput.nativeElement.focus();
+  }
+
+  inputChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.value = inputElement.value;
+    this.inputEvent.emit(event);
+  }
 }
