9b86e22d509014ba8df342f860e938ac70e3b635
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-23 16:28:41 +0000

    create toast message component

---
+++ src/app/components/toast-message/toast-message.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast-message',
+  imports: [],
+  templateUrl: './toast-message.component.html',
+  styleUrl: './toast-message.component.scss',
+})
+export class ToastMessageComponent {}
