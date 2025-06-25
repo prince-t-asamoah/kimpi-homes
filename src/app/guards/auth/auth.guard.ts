e080b38eb8d0b23f722f178bc240d1022c72fa7b
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-06-25 16:46:06 +0000

    update guard to use store auth state

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,12 +1,16 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../../features/auth/services/auth.service';
+import { firstValueFrom } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectIsAuthenticated } from '../../features/auth/auth.store.reducers';
 
-export const authGuard: CanActivateFn = () => {
-  const authService = inject(AuthService);
+export const authGuard: CanActivateFn = async () => {
+  const store = inject(Store);
   const router = inject(Router);
+  const isAuthenticated$ = store.select(selectIsAuthenticated);
+  const isAuthenticated = await firstValueFrom(isAuthenticated$);
 
-  if (!authService.isAuthenticated) {
+  if (!isAuthenticated) {
     return router.createUrlTree(['/login']);
   }
   return true;
